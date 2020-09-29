52388d6f884843c615bc10b56d8e2f69516f7225
Bill Mårtensson <bill@magictechnology.se>
2020-09-29 14:05:48 +0200

    Start

---
+++ SwiftCollectionview/RutaCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RutaCollectionViewCell.swift
+//  SwiftCollectionview
+//
+//  Created by Bill Martensson on 2020-09-29.
+//
+
+import UIKit
+
+class RutaCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+}
--- SwiftCollectionview/ViewController.swift
+++ SwiftCollectionview/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "rutan", for: indexPath) as! RutaCollectionViewCell
+        
+        cell.theLabel.text = String(indexPath.item)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.frame.width/3, height: collectionView.frame.width/3)
+        
+    }
+    
 }
 
